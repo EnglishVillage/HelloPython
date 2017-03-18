c3bdce61faebcb6bf6b6d5537480fa15b0ff540a
EnglishVillage <wkz350623@gmail.com>
2017-03-19 00:22:35 +0800

    wkzpython

--- wkzpackage/HiPython.py
+++ wkzpackage/HiPython.py
@@ -4,15 +4,15 @@
 import math
 import functools
 import sys
-try:
-    import cStringIO as StringIO
-except ImportError: # 导入失败会捕获到ImportError
-    import StringIO
-try:
-    import json # python >= 2.6
-except ImportError:
-    import simplejson as json # python <= 2.5
 
+try:
+	import cStringIO as StringIO
+except ImportError:  # 导入失败会捕获到ImportError
+	import StringIO
+try:
+	import json  # python >= 2.6
+except ImportError:
+	import simplejson as json  # python <= 2.5
 
 'a test module'
 
@@ -28,8 +28,9 @@ def test():
 	else:
 		print 'Too many arguments!'
 
-if __name__ == '__main__':
-	test()
+
+# if __name__ == '__main__':
+	# test()
 
 
 
@@ -37,7 +38,15 @@ if __name__ == '__main__':
 # # print name
 # print "hello python", 213l
 # print "ds", 23
-#
+
+
+# birth = int(raw_input('birth: '))
+# if birth < 2000:
+#     print '00前'
+# else:
+#     print '00后'
+
+
 # a = 100
 # if a >= 0:
 # 	print a
@@ -135,9 +144,105 @@ if __name__ == '__main__':
 # 	print k, v
 
 
+
+# dict和set中key元素不可以包含list对象,因为key是不可变的,list是可变
+# dict={(1, 2, 3):"a",(1, [2, 3]):"2"}	#TypeError: unhashable type: 'list'
+# print dict
+# set=set([(1, 2, 3),(1, [2, 3])])	#TypeError: unhashable type: 'list'
+# print set
+
+
+
 # 遍历得到一个元组(元组包含角标和元素)
 # for _ in enumerate(['A', 'B', 'C']):
 # 	print _
+
+
+# def fib(max):
+# 	n, a, b = 0, 0, 1
+# 	while n < max:
+# 		print b
+# 		# 下面那句等于底下3句[囧]
+# 		# a, b = b, a + b
+# 		temp = a + b
+# 		a = b
+# 		b = temp
+# 		n = n + 1
+
+# fib(6)
+
+
+
+
+
+# #生成器:
+# def fib2(max):
+# 	n, a, b = 0, 0, 1
+# 	while n < max:
+# 		yield b
+# 		a, b = b, a + b
+# 		n = n + 1
+
+# for i in fib2(6):
+# 	print i
+
+
+
+# 高阶函数
+# def add(x,y,f):
+# 	return f(x)+f(y)
+#
+# print add(-5,6,abs)
+
+
+# 将数字转化为字符串,再转化为字符串
+# print map(int,map(str,range(1,10)))
+
+# print sum(range(11),2)
+# map里面的函数只接收一个输入参数
+# reduce里面的函数只接收2个输入参数
+# print reduce(lambda x, y: x + y, map(lambda x: x+10, range(11)))
+# 首字母大写,其它小写
+# print map(lambda x:x[:1].upper()+x[1:].lower(),['adam', 'LISA', 'barT'])
+# print reduce(lambda x, y: x * y, range(1, 5))
+
+
+
+
+# print filter(lambda x:x%2==0,range(11))
+
+
+
+# print sorted([1,2,5,2,3,4])
+
+
+
+
+# def reversed_cmp(x, y):
+#     if x > y:
+#         return -1
+#     if x < y:
+#         return 1
+#     return 0
+# cmp是判断如何比较大小
+# print sorted([36, 5, 12, 9, 21], cmp=reversed_cmp)
+# reverse:True降序,False升序(默认)
+# print sorted([36, 5, 12, 9, 21], reverse=True)
+
+
+
+
+# def cmp_ignore_case(s1, s2):
+#     u1 = s1.upper()
+#     u2 = s2.upper()
+#     if u1 < u2:
+#         return -1
+#     if u1 > u2:
+#         return 1
+#     return 0
+# #忽略大小写进行字符串排序
+# print sorted(['bob', 'about', 'Zoo', 'Credit'],cmp=cmp_ignore_case)
+
 
 # def sum(*args):
 # 	sum = reduce(lambda x, y: x + y, args)
@@ -145,12 +250,17 @@ if __name__ == '__main__':
 #
 # print sum(*range(11))
 
+
+
+
+
+
+
 # def lazy_sum(*args):
 # 	def sum2():
 # 		sum = reduce(lambda x, y: x + y, args)
 # 		return sum
 # 	return sum2
-
 
 # print lazy_sum(*range(11))
 # <function sum2 at 0x0000000002E7FCF8>
@@ -160,6 +270,9 @@ if __name__ == '__main__':
 
 # print lazy_sum(*range(11))==lazy_sum(*range(11))
 # False
+
+
+
 
 # 闭包中返回的函数引用了变量i,所以值相同
 # def count():
@@ -174,7 +287,10 @@ if __name__ == '__main__':
 # print f1(),f2(),f3()
 # 9 9 9
 
-# 闭包中返回的函数引用了具体值
+
+
+
+# 闭包中返回的函数引用了具体值,所以值不同
 # def count2():
 # 	ds=[]
 # 	for i in range(1,4):
@@ -190,20 +306,6 @@ if __name__ == '__main__':
 # 1 4 9
 
 
-# def dec(func):
-# 	def wrapper(*args, **kv):
-# 		print "zzz %s():" % func.__name__
-# 		return func(*args, **kv)
-# 	return wrapper
-# 第1种:
-# @dec
-# def now():
-# 	print '2017-03-17'
-# print now()
-# 第2种
-# def now():
-# 	print '2017-03-17'
-# print dec(now)()
 
 
 
@@ -249,4 +351,19 @@ if __name__ == '__main__':
 
 # 偏函数
 # int2= functools.partial(int, base=2)
-# print int2('110000')
+# # print int2('110000')
+# print int2('110000',base=16)
+
+
+class Student(object):
+	def __init__(self, name, score):
+		self.name = name
+		self.score = score
+
+	def print_score(self):
+		print "%s:%s" % (self.name, self.score)
+
+bart=Student('Bart Simpson', 59)
+lisa=Student('Lisa Simpson', 87)
+bart.print_score()
+lisa.print_score()
